ca172b385bb8d79bfe9b062674282ff2c1d25cd4
ethanrgibson <ethanrgibson33@gmail.com>
2025-04-25 11:05:31 -0600

    first init

---
+++ server/Globals.cs
@@ -0,0 +1,16 @@
+global using System;
+global using System.Data;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Threading.Tasks;
+global using Dapper;
+global using CodeWorks.Utils;
+
+// APPLICATION SPECIFIC
+global using keeper_final.Repositories;
+global using keeper_final.Services;
+global using keeper_final.Models;
