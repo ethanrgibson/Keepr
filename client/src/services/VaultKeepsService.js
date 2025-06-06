c682c97a7ec2f113f1bb7f83717cd7a7d99de13b
ethanrgibson <ethanrgibson33@gmail.com>
2025-06-06 12:32:49 -0600

    fixed some bugs

--- client/src/services/VaultKeepsService.js
+++ client/src/services/VaultKeepsService.js
@@ -1,7 +1,7 @@
 import { api } from "./AxiosService.js"
 import { VaultKeepKept } from "@/models/VaultKeep.js"
 import { AppState } from "@/AppState.js"
-import { Keep } from "@/models/Keep.js"
+
 import { logger } from "@/utils/Logger.js"
 
 class VaultKeepsService {
